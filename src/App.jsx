bd7b678c976e283f6dc56fcf81287abfc1dd9f8e
“tejasvenu45” <tenacioustejas@gmail.com>
2024-04-22 17:54:00 +0530

    Proper one

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+function App() {
+
+
+
+  return (
+    <>
+    <div>
+    <h1 className="text-3xl bg-blue-600 text-center w-full">
+        Hello World 
+
+    </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
